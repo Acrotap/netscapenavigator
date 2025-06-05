32143600c6c7fc58a0950ee20c9a53c979de03e0
Acrotap <78327439+Acrotap@users.noreply.github.com>
2025-06-05 08:45:24 -0700

    Update and rename example-module.js to netscapenavigator.js

---
+++ netscapenavigator.js
@@ -0,0 +1,3 @@
+(async function exampleModule() {
+    $window({title: "Netscape - [Version 2.02]", url: "https://theoldnet.com/browser/", icon: "https://theoldnet.com/browser/favicon.ico", width: 880, height: 500, center: true})
+})
