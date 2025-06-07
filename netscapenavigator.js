591918e2d909a109126ad097fffa1589b50fcc69
Acrotap <78327439+Acrotap@users.noreply.github.com>
2025-06-06 19:28:05 -0700

    Update netscapenavigator.js

--- netscapenavigator.js
+++ netscapenavigator.js
@@ -1,3 +1,3 @@
-(async function exampleModule() {
+(async function netscapenavigator() {
     $window({title: "Netscape - [Version 2.02]", url: "https://theoldnet.com/browser/", icon: "https://theoldnet.com/browser/favicon.ico", width: 880, height: 500, center: true})
 })
